35194a28c4e2acea8c96ca4982a5755abb54e8b0
Marc DiFilippo <marc@marcdif.com>
2022-01-02 20:58:57 -0500

    Remove python-agent

--- python-agent/clear.py
+++
@@ -1,4 +0,0 @@
-from handlers.color import Color
-from handlers.lightstrip import *
-
-set_full_strip(Color(0, 0, 0))
--- python-agent/handlers/bpm.py
+++
@@ -1,6 +0,0 @@
-class BPM:
-    def __init__(self, bpm, pulses, noteval) -> None:
-        self.bpm = bpm
-        self.pulses = pulses
-        self.noteval = noteval
-        pass
